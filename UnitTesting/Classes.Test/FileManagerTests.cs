f7fed6e40e62e6e534a96e74ff85ce0c343579b1
Radosław <57417179+rabranto@users.noreply.github.com>
2021-09-21 20:42:30 +0200

    New Tests create with AAA method and Exception handling

--- UnitTesting/Classes.Test/FileManagerTests.cs
+++ UnitTesting/Classes.Test/FileManagerTests.cs
@@ -24,21 +24,51 @@ namespace Classes.Test
         [TestMethod]
         public void FileNameDoesNotExist()
         {
-            Assert.Inconclusive();
+            //Arrange
+            FileManager fileManager = new FileManager();
+            bool fromCall;
+
+            //Act
+            fromCall = fileManager.IsFileExist(@"C:\Windows\ghost_file.exe");
+
+            //Assert
+            Assert.IsFalse(fromCall);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmpty_UsingAtribute()
         {
-            Assert.Inconclusive();
+            //Arrange
+            FileManager fileManager = new FileManager();
+            bool fromCall;
+
+            //Act
+            fromCall = fileManager.IsFileExist("");
+                        
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmpty_UsingTryCatch()
         {
-            Assert.Inconclusive();
+            //Arrange
+            FileManager fileManager = new FileManager();
+
+
+            //Act
+            try
+            {
+                fileManager.IsFileExist("");
+            }
+
+            catch (ArgumentNullException)
+            {
+                return;
+            }
+            //Assert
+            Assert.Fail("Call to IsFileExist() did NOT throw an ArgumentNullException.");
+
         }
     }
 }
